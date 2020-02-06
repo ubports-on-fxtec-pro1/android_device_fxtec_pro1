b71de9a1307b094263b6d640a6b87bac4ab8bb18
NeKit <nekit1000@gmail.com>
2020-02-07 00:24:57 +0100

    Make systemimage compile on Halium 9
    Change-Id: Ieedfcb4954e57f77db078dc354fde5162ecbf12e

--- lineage_pro1.mk
+++ lineage_pro1.mk
@@ -16,13 +16,14 @@
 
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+#$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/halium.mk)
 
 # Inherit from pro1 device
 $(call inherit-product, device/fxtec/pro1/device.mk)
 
 # Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+#$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 PRODUCT_NAME := lineage_pro1
 PRODUCT_DEVICE := pro1
